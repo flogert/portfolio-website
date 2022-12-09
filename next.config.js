710068c51a874cb5de1dfd343d7cc53caa23e798
Flogert Bardhi <flogert@rocketmail.com>
2022-12-08 21:48:00 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
