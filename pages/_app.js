13851a7a5f8b250dd0c53cdc0d9f31f2432c1090
Flogert Bardhi <flogert@rocketmail.com>
2022-12-08 22:51:57 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
-config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
